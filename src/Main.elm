70e40c51ebf8e70d29ae70395557500a432aad90
geppettodivacin <ericdilmore@gmail.com>
2017-08-12 13:24:10 -0500

    Refactor and add a demonstration at the bottom.

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,9 @@
 import Http
-import Native
+import Html exposing (..)
+import Html.Events exposing (..)
 import Json.Decode as Decode
 import Json.Encode as Encode
+
 
 type alias Config =
     { host : String
@@ -11,37 +13,127 @@ type alias Config =
 
 fetch :
   Decode.Decoder a
-  -> (Result Http.Error a -> msg)
   -> Config
   -> String
-  -> Cmd msg
-fetch decoder toMsg config key =
+  -> Http.Request a
+fetch decoder config key =
   let
     fetchAddr =
       configString config ++ "/" ++ key
   in
-      Http.send toMsg (Http.get fetchAddr decoder)
+    Http.get fetchAddr decoder
 
 
 store :
   (a -> Encode.Value)
-  -> msg
   -> Config
   -> String
   -> a
-  -> Maybe String
-  -> Cmd msg
-store encode msg config key value revision =
+  -> Http.Request String
+store encode config key value =
   let
     postAddr =
       configString config ++ "/" ++ key
-    msg = Encode.encode 0 (encode value |> withRev revision)
+    body = Http.jsonBody (encode value)
   in
-    Cmd.none
+    Http.post postAddr body Decode.string
 
-withRev : Maybe String -> Encode.Value -> Encode.Value
-withRev = Native.withRev
 
 configString : Config -> String
 configString config =
-  config.host ++ config.db
+  "http://" ++ config.host ++ "/" ++ config.db
+
+
+
+--
+-- Demonstration
+--
+
+
+main =
+  Html.program
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = \_ -> Sub.none
+    }
+
+
+-- MODEL
+
+
+type alias Model =
+  { doc : Maybe Document
+  }
+
+
+type alias Document =
+  { id : String
+  , rev : String
+  , value : String
+  }
+
+
+docDecoder : Decode.Decoder Document
+docDecoder =
+  Decode.map3 Document
+    (Decode.field "_id" Decode.string)
+    (Decode.field "_rev" Decode.string)
+    (Decode.field "value" Decode.string)
+
+
+init =
+  (Model Nothing, Cmd.none)
+
+
+-- UPDATE
+
+
+type Msg
+  = Retrieved Document
+  | Error 
+  | Fetch
+
+
+fetchCmd : Cmd Msg
+fetchCmd =
+  let
+    config = Config "localhost:5984" "elm-example"
+
+    id = "fetchdoc"
+
+    req = fetch docDecoder config id
+
+    parseResult result =
+      case result of
+        Ok doc ->
+          Retrieved doc
+
+        Err error ->
+          Debug.log (toString error) error |> always Error
+  in
+    Http.send parseResult req
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    Retrieved doc ->
+      ({model | doc = Just doc}, Cmd.none)
+    
+    Fetch ->
+      (model, fetchCmd)
+
+    Error ->
+      ({model | doc = Nothing}, Cmd.none)
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ h1  [] [ text (toString model.doc) ]
+    , button [ onClick Fetch ] [ text "Fetch" ]
+    ]
